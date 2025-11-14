b8f1fdc8f23a377ee4da3f7f414f72b0ad3a8ae8
Dominik Könitzer <dominik.koenitzer@gmail.com>
2025-11-14 17:04:25 +0100

    Set default dev server port to 1000

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 1000,
+  },
   build: {
     rollupOptions: {
       output: {
