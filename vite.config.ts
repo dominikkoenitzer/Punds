5fe5668296d88e74ff30ef389d3ed822d1cd5021
Dominik Könitzer <dominik.koenitzer@gmail.com>
2025-11-14 17:02:53 +0100

    Add code splitting configuration to reduce bundle size

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+          'ui-vendor': ['@chakra-ui/react', '@chakra-ui/theme', '@chakra-ui/theme-tools'],
+          'animation-vendor': ['framer-motion'],
+          'emotion-vendor': ['@emotion/react', '@emotion/styled'],
+          'icons-vendor': ['react-icons'],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 600,
+  },
 })
