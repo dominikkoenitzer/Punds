fd9c738275d8dd66532cf102ed484feb4bdf6a34
Dominik Könitzer <dominik.koenitzer@gmail.com>
2025-11-17 10:32:50 +0100

    feat: Integrate Vercel Analytics and enhance SEO prevention in index.html
    - Added Vercel Analytics to main.tsx for improved tracking.
    - Updated index.html with multiple meta tags to prevent indexing by various bots.
    - Simplified App component by removing ChakraProvider wrapper.

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
+import { Analytics } from '@vercel/analytics/react'
 import App from './App'
 import theme from './theme'
 import './index.css'
+import './App.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <App />
+      <Analytics />
     </ChakraProvider>
   </React.StrictMode>,
 )
